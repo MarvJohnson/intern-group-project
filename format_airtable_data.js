484ff16d5c54cac83f50604ce897c52df192fe2b
Marvel Johnson <51891409+MarvJohnson@users.noreply.github.com>
2022-08-02 11:46:23 -0700

    Update format_airtable_data.js

--- format_airtable_data.js
+++ format_airtable_data.js
@@ -2,6 +2,9 @@ let [,, quote, phoneNumbers] = process.argv;
 
 const removeEscapeCharacters = (value) => quote.replace(/\\/g, '');
 
+console.log(quote);
+console.log(phoneNumbers);
+
 quote = JSON.parse(removeEscapeCharacters(quote)).records[0].fields.quote;
 phoneNumbers = JSON.parse(removeEscapeCharacters(phoneNumbers)).records[0].fields.number;
 
