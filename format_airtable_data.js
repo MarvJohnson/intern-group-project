8d71cac6059e052e01bde924d3409cc1e5020307
Marvel Johnson <51891409+MarvJohnson@users.noreply.github.com>
2022-08-08 11:39:42 -0700

    Update format_airtable_data.js

--- format_airtable_data.js
+++ format_airtable_data.js
@@ -1,5 +1,7 @@
 let [,, quote, phoneNumbers, quoteIndex] = process.argv;
 
+console.log(quoteIndex);
+
 quoteIndex = Number(quoteIndex);
 quote = JSON.parse(quote).records[quoteIndex].fields.quote;
 phoneNumbers = JSON.parse(phoneNumbers).records.map(record => record.fields.number);
